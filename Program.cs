89f732676211fb71d84d50bbb971ba3541280542
MrQugen <mrqugen@gmail.com>
2023-11-05 15:20:10 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SoftwareImplementationOfTheHashingAlgorithm;
+
+public static class Program
+{
+    public static void Main()
+    {
+        const string input1 = "Hello, World!";
+        const string input2 = "This is a test message.";
+        const string input3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+        var data1 = Encoding.UTF8.GetBytes(input1);
+        var data2 = Encoding.UTF8.GetBytes(input2);
+        var data3 = Encoding.UTF8.GetBytes(input3);
+
+        // Ваша реалізація SHA-1
+        var customHash1 = Sha1Hash.ComputeSha1(data1);
+        var customHash2 = Sha1Hash.ComputeSha1(data2);
+        var customHash3 = Sha1Hash.ComputeSha1(data3);
+
+        // Бібліотечна реалізація SHA-1
+        var systemHash1 = CalculateSha1HashSystem(data1);
+        var systemHash2 = CalculateSha1HashSystem(data2);
+        var systemHash3 = CalculateSha1HashSystem(data3);
+
+        // Порівнюємо геш-значення
+        var match1 = CompareHashes(customHash1, systemHash1);
+        var match2 = CompareHashes(customHash2, systemHash2);
+        var match3 = CompareHashes(customHash3, systemHash3);
+
+        Console.WriteLine("Message 1 Hashes Match: " + match1);
+        Console.WriteLine("Message 2 Hashes Match: " + match2);
+        Console.WriteLine("Message 3 Hashes Match: " + match3);
+        
+        var input = "Hello, World!";
+        var data = Encoding.UTF8.GetBytes(input);
+
+        // Проводимо вимірювання часу для вашої реалізації SHA-1
+        var customHashStopwatch = new Stopwatch();
+        customHashStopwatch.Start();
+        var customHash = Sha1Hash.ComputeSha1(data);
+        customHashStopwatch.Stop();
+        var customHashTime = customHashStopwatch.ElapsedMilliseconds;
+
+        // Проводимо вимірювання часу для бібліотечної реалізації SHA-1
+        var systemHashStopwatch = new Stopwatch();
+        systemHashStopwatch.Start();
+        var systemHash = CalculateSha1HashSystem(data);
+        systemHashStopwatch.Stop();
+        var systemHashTime = systemHashStopwatch.ElapsedMilliseconds;
+
+        Console.WriteLine("SHA-1 Hash (Custom Implementation): " + Sha1Hash.BytesToHex(customHash));
+        Console.WriteLine("SHA-1 Hash (System Implementation): " + Sha1Hash.BytesToHex(systemHash));
+
+        Console.WriteLine("Time taken by Custom Implementation: " + customHashTime + " milliseconds");
+        Console.WriteLine("Time taken by System Implementation: " + systemHashTime + " milliseconds");
+
+        Console.WriteLine(CompareHashes(customHash, systemHash) ? "Hashes Match!" : "Hashes Do Not Match!");
+    }
+
+    private static byte[] CalculateSha1HashSystem(byte[] data)
+    {
+        using var sha1 = SHA1.Create();
+        return sha1.ComputeHash(data);
+    }
+
+    private static bool CompareHashes(IReadOnlyList<byte> hash1, IReadOnlyList<byte> hash2)
+    {
+        if (hash1.Count != hash2.Count)
+            return false;
+
+        for (var i = 0; i < hash1.Count; i++)
+        {
+            if (hash1[i] != hash2[i])
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Sha1Hash.cs
@@ -0,0 +1,129 @@
+﻿using System.Text;
+
+namespace SoftwareImplementationOfTheHashingAlgorithm;
+
+public static class Sha1Hash
+{
+    public static byte[] ComputeSha1(byte[] data)
+    {
+        // Ініціалізуємо початкові значення H0-H4, які визначені в специфікації SHA-1.
+        var h = new uint[5];
+        h[0] = 0x67452301;
+        h[1] = 0xEFCDAB89;
+        h[2] = 0x98BADCFE;
+        h[3] = 0x10325476;
+        h[4] = 0xC3D2E1F0;
+
+        // Перетворюємо вхідні дані в байти
+        var paddedData = PadData(data);
+
+        // Обробляємо кожний блок даних
+        for (var i = 0; i < paddedData.Length; i += 64)
+        {
+            var block = new byte[64];
+            Array.Copy(paddedData, i, block, 0, 64);
+            ProcessBlock(h, block);
+        }
+
+        // Кінцеві значення хешу
+        var result = new byte[20];
+        for (var i = 0; i < 5; i++)
+        {
+            result[i * 4] = (byte)(h[i] >> 24);
+            result[i * 4 + 1] = (byte)(h[i] >> 16);
+            result[i * 4 + 2] = (byte)(h[i] >> 8);
+            result[i * 4 + 3] = (byte)h[i];
+        }
+
+        return result;
+    }
+
+    private static byte[] PadData(byte[] data)
+    {
+        var originalLength = data.Length;
+        var originalBits = (long)originalLength * 8;
+        var padLength = originalLength % 64 < 56 ? 56 - originalLength % 64 : 120 - originalLength % 64;
+        var paddedData = new byte[originalLength + padLength + 8];
+        Array.Copy(data, paddedData, originalLength);
+        paddedData[originalLength] = 0x80; // Додаємо байт 0x80
+        for (var i = 0; i < 8; i++)
+        {
+            paddedData[paddedData.Length - 8 + i] = (byte)(originalBits >> (56 - i * 8));
+        }
+        return paddedData;
+    }
+
+    private static void ProcessBlock(uint[] h, byte[] block)
+    {
+        var w = new uint[80];
+
+        // Розширюємо блок 16 слівами в 80 слів
+        for (var t = 0; t < 16; t++)
+        {
+            w[t] = (uint)(block[t * 4] << 24) |
+                   (uint)(block[t * 4 + 1] << 16) |
+                   (uint)(block[t * 4 + 2] << 8) |
+                   block[t * 4 + 3];
+        }
+        for (var t = 16; t < 80; t++)
+        {
+            w[t] = LeftRotate(w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16], 1);
+        }
+
+        // Ініціалізація тимчасових змінних
+        var a = h[0];
+        var b = h[1];
+        var c = h[2];
+        var d = h[3];
+        var e = h[4];
+
+        // Основний цикл
+        for (var t = 0; t < 80; t++)
+        {
+            var temp = LeftRotate(a, 5) + F(t, b, c, d) + e + w[t] + K(t);
+            e = d;
+            d = c;
+            c = LeftRotate(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        // Оновлюємо значення хешу
+        h[0] = h[0] + a;
+        h[1] = h[1] + b;
+        h[2] = h[2] + c;
+        h[3] = h[3] + d;
+        h[4] = h[4] + e;
+    }
+
+    private static uint F(int t, uint b, uint c, uint d)
+    {
+        return t switch
+        {
+            < 20 => (b & c) | ((~b) & d),
+            < 40 => b ^ c ^ d,
+            < 60 => (b & c) | (b & d) | (c & d),
+            _ => b ^ c ^ d
+        };
+    }
+
+    private static uint K(int t)
+    {
+        return t < 20 ? 0x5A827999 : t < 40 ? 0x6ED9EBA1 : t < 60 ? 0x8F1BBCDC : 0xCA62C1D6;
+    }
+
+    private static uint LeftRotate(uint value, int offset)
+    {
+        return (value << offset) | (value >> (32 - offset));
+    }
+
+    public static string BytesToHex(byte[] bytes)
+    {
+        var hex = new StringBuilder(bytes.Length * 2);
+        foreach (var b in bytes)
+        {
+            hex.Append($"{b:x2}");
+        }
+        return hex.ToString();
+    }
+}
